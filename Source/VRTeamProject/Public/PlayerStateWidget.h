6d8a999aaf477d67e485b2c09b547e52e9b3816a
telletobe <tlatjsqh159@naver.com>
2025-06-23 11:50:41 +0900

    Update PlayerStateWidget.h

--- Source/VRTeamProject/Public/PlayerStateWidget.h
+++ Source/VRTeamProject/Public/PlayerStateWidget.h
@@ -1,45 +1,44 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "Blueprint/UserWidget.h"
-#include "PlayerStateWidget.generated.h"
-
-class UProgressBar;
-class UButton;
-class APlayerCharacter;
-/**
- * 
- */
-UCLASS()
-class VRTEAMPROJECT_API UPlayerStateWidget : public UUserWidget
-{
-	GENERATED_BODY()
-	
-public:
-
-	UFUNCTION()
-	void UpdatePlayerHP(float CurrentHp,float MaxHp);
-	void UpdatePlayerDef();
-	void UpdatePlayerExp(float CurrentExp);
-
-protected:
-	virtual void NativeConstruct() override;
-
-private:
-	UPROPERTY(meta = (Bindwidget))
-	TObjectPtr<UProgressBar> HPBar;
-
-	UPROPERTY(meta = (Bindwidget))
-	TObjectPtr<UProgressBar> DefBar;
-
-	UPROPERTY(meta = (Bindwidget))
-	TObjectPtr<UProgressBar> ExpBar;
-
-	UPROPERTY()
-	TObjectPtr<APlayerCharacter> Player;
-
-	// header
-
-};
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "Blueprint/UserWidget.h"
+//#include "PlayerStateWidget.generated.h"
+//
+//class UProgressBar;
+//class UButton;
+//class APlayerCharacter;
+///**
+// * 
+// */
+//UCLASS()
+//class VRTEAMPROJECT_API UPlayerStateWidget : public UUserWidget
+//{
+//	GENERATED_BODY()
+//	
+//public:
+//
+//	UFUNCTION()
+//	void UpdatePlayerHP(float CurrentHp,float MaxHp);
+//	void UpdatePlayerDef();
+//	void UpdatePlayerExp(float CurrentExp);
+//
+//protected:
+//	virtual void NativeConstruct() override;
+//
+//private:
+//	UPROPERTY(meta = (Bindwidget))
+//	TObjectPtr<UProgressBar> HPBar;
+//
+//	UPROPERTY(meta = (Bindwidget))
+//	TObjectPtr<UProgressBar> DefBar;
+//
+//	UPROPERTY(meta = (Bindwidget))
+//	TObjectPtr<UProgressBar> ExpBar;
+//
+//	UPROPERTY()
+//	TObjectPtr<APlayerCharacter> Player;
+//
+//
+//};
