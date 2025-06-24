f669ad38273fd923d5d513ceb7a3f20d82ed3250
Dobblin <kdy30317@gmail.com>
2025-06-24 17:10:27 +0900

    Package

--- Build/Android/src/com/YourCompany/VRTeamProject/OBBData.java
+++ Build/Android/src/com/YourCompany/VRTeamProject/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-56973113L // the length of the file in bytes
+57560222L // the length of the file in bytes
 )
 };
 };
